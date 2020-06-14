faf19f0e7bf79544594d3a9ce083d2871b9950bf
PDimitar <dimitar.pelivanov@gmail.com>
2020-06-14 21:36:26 +0200

    Commit done

---
+++ index.R
@@ -0,0 +1,39 @@
+---
+  title: "PP - Projekt 1"
+author: "Dimitar Pelivanov - 89171103"
+date: "6/14/2020"
+output: 
+  html_document
+---
+  
+  ```{r setup, include=FALSE, message=FALSE, error=TRUE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+```{r echo=FALSE}
+df <- read.csv(file = "avocado.csv")
+```
+
+```{r echo=FALSE}
+```
+
+# Uvod
+
+<font size="4">Hi everyone, in this project we will talk about the avocados.Also known as an alligator pear or butter fruit, the versatile avocado is the only fruit that provides a substantial amount of healthy monounsaturated fatty acids (MUFA). Avocados are a naturally nutrient-dense food and contain nearly 20 vitamins and minerals.
+</font>
+  
+  <font size="4">
+  It is a well known fact that Millenials LOVE Avocado Toast. It's also a well known fact that all Millenials live in their parents basements.
+
+Clearly, they aren't buying home because they are buying too much Avocado Toast!
+  
+  But maybe there's hope… if a Millenial could find a city with cheap avocados, they could live out the Millenial American Dream.
+</font>
+
+<font size="4">
+The main focus for this project will be analyzing the data from our avocado sales in the paste few years in different regions.
+</font>
+
+
+# Predstavitev in opis problema
+
